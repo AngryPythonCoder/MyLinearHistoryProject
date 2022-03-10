574bf375924a96f72a5f2f245922c1a02d9abe77
Kirill Kirnosov <kirx2@yandex.ru>
2022-03-10 22:43:23 +0400

    Added fib function that recursively calculates nth Fibonacci number

--- code.cpp
+++ code.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+int fib(int n) {
+    if (n > 2) return fib(n-1) + fib(n-2);
+    else return 1;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
+    cout << fib(10) << endl;
 
     return 0;
 }
